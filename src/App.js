2b33c7b32818d1735dcfdd1eb4730de762742c18
David Bolton <smdeox@live.com>
2020-01-27 15:12:53 -0500

    Added JSX to toggle data

--- src/App.js
+++ src/App.js
@@ -60,18 +60,11 @@ state = {
       padding: '8px',
       cursor: 'pointer'
     };
-     return (
-      <div className="App">
-        <h1>David's React App</h1>
-        <p>This is working!</p>
-        <button 
-        style={style}
-        onClick={ this.togglePersonsHandler}>Toggle Persons
-        </button>
-        
-        { 
-        this.state.showPersons ? 
-          <div>
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+      <div>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}/> 
@@ -85,8 +78,19 @@ state = {
         age={this.state.persons[2].age}
         click={() => this.switchNameHandler('Testing123')}
         />
-        </div> : null
-        }
+        </div>
+      );
+    }
+     return (
+      <div className="App">
+        <h1>David's React App</h1>
+        <p>This is working!</p>
+        <button 
+        style={style}
+        onClick={ this.togglePersonsHandler}>Toggle Persons
+        </button>
+      {persons}
+        
       </div>
       ); 
       //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Heyyy'))
