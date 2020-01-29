f6c4c6c7bfb36f9b840d876e10801a1e452eb64b
David Bolton <smdeox@live.com>
2020-01-28 21:02:03 -0500

    Added a delete persons function

--- src/App.js
+++ src/App.js
@@ -52,6 +52,12 @@ state = {
     })
   }
 
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -65,8 +71,9 @@ state = {
     if (this.state.showPersons) {
       persons = (
         <div>
-        {this.state.persons.map(person => {
-          return <Person 
+        {this.state.persons.map((person, index) => {
+          return <Person
+          click={() => this.deletePersonHandler(index)} 
           name={person.name} 
           age={person.age} />
         })}
