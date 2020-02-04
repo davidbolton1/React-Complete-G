143d686dc3d66f3b9c19744fa09e68792331c2a4
David Bolton <smdeox@live.com>
2020-02-03 21:46:37 -0500

    Added lifecycles

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import './Person.css';
 import styled from 'styled-components';
 //import Radium from 'radium';
@@ -22,7 +22,8 @@ text-align: center;
 }
 `
 
-const Person = (props) => {
+class Person extends Component {
+    render() {
     // const style = {
     //     '@media (min-width: 500px)': {
     //         width: '450px'
@@ -32,11 +33,12 @@ const Person = (props) => {
     return ( 
     //< div className="Person" style={style}>
         <StyledDiv>
-        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p> 
-        <p> {props.children} </p> 
-        <input type="text" onChange={props.changed} value={props.name} />
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p> 
+        <p> {this.props.children} </p> 
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
         </StyledDiv>
     )
 };
+}
 // export default Radium(Person); 
 export default Person; 
